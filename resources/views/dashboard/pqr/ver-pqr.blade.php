1431804f88146be80db2bbc1bb12ab05c0b81f27
Leiner Ortega <leicortega@gmail.com>
2020-02-26 17:59:20 -0500

    Mostrar los datos en la plantilla blade de Correo

--- resources/views/dashboard/pqr/ver-pqr.blade.php
+++ resources/views/dashboard/pqr/ver-pqr.blade.php
@@ -73,7 +73,7 @@
                                             </tbody>
                                         </table>
                                     </div>
-
+                                    {{ $correo }}
                                 </div>
                             </div>
                         </div>
